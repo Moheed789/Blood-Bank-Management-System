80071a3bbe4d9aab43b750665ef65b6f098017a1
Moheed789 <moheedsiddiqui74@gmail.com>
2024-04-24 19:39:06 +0500

    Create Post Confirmation lambda function trigger and return save user patient group

--- functions/manage_patient/postConformation.js
+++ functions/manage_patient/postConformation.js
@@ -1,15 +1,17 @@
 const AWS = require("aws-sdk");
 
 const handler = async(event) => {
+  console.log("event", JSON.stringify(event));
     try {
         const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider();
         const params = {
           GroupName: 'patient',
           Username: event.userName,
-          UserPoolId: event.userpoolId
+          UserPoolId: event.userPoolId
         };
         let res =await cognitoIdentityServiceProvider.adminAddUserToGroup(params).promise();
         console.log('User added to group successfully.',res );
+        return event
       } catch (err) {
         console.error('Error adding user to group:', err);
       }
